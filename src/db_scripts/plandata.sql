671bb074f2ae6744bd5b06e3d471ece6e44ef00b
kaustubh.dhokte <kaustubh.dhokte@gmail.com>
2020-10-20 13:41:52 -0400

    sql script to populate plans data

---
+++ src/db_scripts/plandata.sql
@@ -0,0 +1,2 @@
+insert into plans (id, name, price, duration, requestslimit) values (1, 'basic', 0.0, 90, 100);
+insert into plans (id, name, price, duration, requestslimit) values (2, 'premium', 100.0, 365, 500);
