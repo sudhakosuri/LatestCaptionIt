725c1a649418c5be7bbb2830c7e52eda42cfa951
kaustubh.dhokte <kaustubh.dhokte@gmail.com>
2020-11-30 01:57:37 -0500

    working authenticate with dbutils zip

--- src/lambda-functions/authenticate.py
+++ src/lambda-functions/authenticate.py
@@ -1,36 +1,14 @@
 import json
 import pymysql
 import logging
+import dbutils
 from random import choice
 from string import ascii_lowercase
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 def lambda_handler(event, context):
-
-    def connect():
-        try:
-            #rds settings
-            rds_host  = "soc-db-instance.cqn1yr2onvp2.us-east-1.rds.amazonaws.com"
-            name = "admin"
-            password = "password1q"
-            db_name = "soc_database"
-
-            conn = None
-            conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=30)
-            logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
-            return conn
-        except pymysql.MySQLError as e:
-            logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-            logger.error(e)
-            return conn
-
-    conn = connect()
-    if not conn:
-        logger.error("Error connecting database")
-        # return value ? check http responses
-        return
 
     # if used with HTTP API, json data is encoded into event["body"]
     # if used with REST API, json data is event
@@ -38,25 +16,39 @@ def lambda_handler(event, context):
         data = json.loads(event["body"])
     except KeyError:
         data = event
-
-    with conn.cursor() as cursr:
+        
+    try:
         email = data["email"]
         password = data["password"]
-        db_pass = None
-        query = f'select id from users where email="{email}" and password="{password}"'
-        logger.debug(query)
-        try:
-            cursr.execute(query)
-            userid = cursr.fetchone()
-            if userid:
-                status = 200
-                message = {"id": userid[0]}
-            else:
-                status = 404
-                message = {"message": "Incorrect username or password"}
-        except Exception as ex:
-            logger.error(f"Error fetching user record for user {email}: {ex}")
-    # TODO implement
+    except Exception as ex:
+        error = f'Missing request data: "{ex}"'
+        logger.error(error)
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"message": error}),
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+                },
+            }
+
+    try:
+        userid = dbutils.validate_user(email, password)
+        if userid:
+            status = 200
+            message = {"id": userid}
+            logger.info(f"User with id: {userid} authenticated succesfully!")
+        else:
+            status = 404
+            message = {"message": "Incorrect username or password"}
+            logger.warning("Incorrect username or password")
+    except Exception as ex:
+        error = f"Error fetching user record for user {email}: {ex}"
+        logger.error(error)
+        status = 500
+        message = {"message": error}
+
     return {
         'statusCode': status,
         'body': json.dumps(message),
