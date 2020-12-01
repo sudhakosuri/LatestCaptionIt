f33bc43a99fcb3e868d8b39a1e0bf5bbd713730e
Atharva <atharvamuley1@gmail.com>
2020-11-30 23:38:46 -0500

    Update smtp_notifier.py

--- src/lambda-functions/smtp_notifier.py
+++ src/lambda-functions/smtp_notifier.py
@@ -1,16 +1,17 @@
 import smtplib
 import os
+import pymysql
 import logging
+from dbutils import connect
 
-def build_message():
+def build_message(sender, user_name, user_email, plan_info):
 
-    message = f""" From: {sender}
-    To: {receiver}
-    MIME-Version: 1.0
-    Content-Type: text/html
-    Subject: Note:
-
-    {body}
+    message = f"""
+    Subject: CaptionIt API Plan Exhaustion
+    
+    Hello {user_name},
+    
+    Your current CaptionIt {plan_info} Plan is about to expire. Please, purchase a new plan to avoid discontinuation of the Service.
 
     Regards,
     CaptionIt Team
@@ -18,18 +19,19 @@ def build_message():
 
     return message
 
-def send_notification():
-
+def send_notification(host, port, username, password, receiver, message):
+    
     try:
         server = smtplib.SMTP(host, port)
         server.ehlo()
         server.starttls()
         server.login(username, password)
-        server.sendmail(sender, receiver, message)
+        server.sendmail(username, receiver, message)
         server.close()
         return True
-    except:
+    except Exception as e:
         # Log exception
+        print("Exception:", e)
         return False
         
 
@@ -38,22 +40,38 @@ def lambda_handler(event, context):
     host = os.environ['SMTPHOST']
     port = os.environ['SMTPPORT']
     sender = os.environ['SENDER']
-
+    password = os.environ['SENDER_PASSWORD']
     response = {
         "isBase64Encoded": False
     }
 
     # Check if there is a username, user_email and plan info in event
     try:
-        username = event['username']
+        user_name = event['user_name']
         user_email = event['user_email']
         plan_info = event['plan_info']
     except Exception as e:
         # Log exception here
         response['statusCode'] = 500
         return response
+    conn = connect()
+    if not conn:
+        # Logg error
+        return None
+    with conn.cursor() as crsur:
+        query = 'select u.email, p.name from users u, plans p where u.planid=p.id'
+        try:
+            crsur.execute(query)
+            details = crsur.fetchall()
 
-    send_status = send_notification()
+        except Exception as e:
+            print(e)
+            return None
+    for detail in details:
+        receiver = detail[0]
+        plain_info = detail[1]
+        message = build_message(sender, user_name, receiver, plan_info)
+        send_status = send_notification(host, port, sender, password, receiver, message)
 
     if send_status:
         response['statusCode'] = 200
@@ -61,4 +79,3 @@ def lambda_handler(event, context):
         response['statusCode'] = 400
 
     return response
-
