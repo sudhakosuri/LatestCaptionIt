5dcb85b20e3589c82b02e9bee1058473c55d2d43
kaustubh.dhokte <kaustubh.dhokte@gmail.com>
2020-12-01 11:38:42 -0500

    encrypt decrypt logic

--- src/lambda-functions/dbutils.py
+++ src/lambda-functions/dbutils.py
@@ -1,5 +1,6 @@
 import pymysql
 import logging
+from cryptography.fernet import Fernet
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -8,6 +9,8 @@ RDS_HOST = "soc-db-instance.cqn1yr2onvp2.us-east-1.rds.amazonaws.com"
 RDS_USER_NAME = "admin"
 RDS_PASSWORD = "password1q"
 RDS_DB_NAME = "soc_database"
+
+CRYPTO_KEY = b'7ItlCQhKCGlc7LoFeNDLjHi__Y-0IWDxhtpcd1RBB1k='
 
 def connect():
     """
@@ -70,7 +73,33 @@ def validate_user_by_token(auth_token):
         logger.error(f"Error fetching user record for token {auth_token}: {ex}")
         raise ex
 
+def encrypt(input_string):
+    """
+    """
+    try:
+        fernet = Fernet(CRYPTO_KEY)
+        cipher = fernet.encrypt(str.encode(input_string))
+        return cipher
+    except Exception as ex:
+        err = f'Error encrypting input string {input_string}:  {ex}'
+        logger.error(err)
+        raise Exception(err)
+
+def decrypt(cipher_string):
+    """
+    """
+    try:
+        fernet = Fernet(CRYPTO_KEY)
+        dcipher = fernet.decrypt(cipher_string).decode()
+        return dcipher
+    except Exception as ex:
+        err = f'Error decrypting cipher {cipher_string}:  {ex}'
+        logger.error(err)
+        raise Exception(err)
+
+
 if __name__ == '__main__':
-    print(connect())
-    print(validate_user('kaustubh@gmail.com', 'Kaustubh123#'))
-    print(validate_user('userthatcanneverexist@gmail.com', 'Kaustubh123'))
+    #print(connect())
+    #print(validate_user('kaustubh@gmail.com', 'Kaustubh123#'))
+    #print(validate_user('userthatcanneverexist@gmail.com', 'Kaustubh123'))
+    print(decrypt(encrypt("Qwerty@1234")))
